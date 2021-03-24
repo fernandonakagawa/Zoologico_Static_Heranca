72e50f3af5decb5c252ef7252e95072436e6cf92
fernandonakagawa <fernando.nakagawa@sesisenaipr.org.br>
2021-03-24 14:12:47 -0300

    Projeto criado

---
+++ SistemaZoologico/SistemaZoologico/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SistemaZoologico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
